e5d52ae660a36a873ada9bc4e5515792ed50660e
azzamaddouri <maddouriazza@gmail.com>
2024-06-04 01:35:34 +0200

    Home Screen Design !

---
+++ lib/model/home_type.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+enum HomeType { aiChatBot, aiImage, aiTranslator }
+
+extension MyHomeType on HomeType {
+  String get title => switch (this) {
+        HomeType.aiChatBot => 'AI ChatBot',
+        HomeType.aiImage => 'AI Image Creator',
+        HomeType.aiTranslator => 'Language Translator',
+      };
+
+  String get lottie => switch (this) {
+        HomeType.aiChatBot => 'ai_hand_waving.json',
+        HomeType.aiImage => 'ai_play.json',
+        HomeType.aiTranslator => 'ai_ask_me.json',
+      };
+
+  bool get leftAlign => switch (this) {
+        HomeType.aiChatBot => true,
+        HomeType.aiImage => false,
+        HomeType.aiTranslator => true,
+      };
+
+  EdgeInsets get padding => switch (this) {
+        HomeType.aiChatBot => EdgeInsets.zero,
+        HomeType.aiImage => const EdgeInsets.all(20),
+        HomeType.aiTranslator => EdgeInsets.zero,
+      };
+}
--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:ai_assistant/helper/global.dart';
 import 'package:ai_assistant/helper/pref.dart';
+import 'package:ai_assistant/model/home_type.dart';
+import 'package:ai_assistant/widget/home_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -22,8 +24,36 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     // Initializing device size
     mq = MediaQuery.sizeOf(context);
-    return const Scaffold(
-      body: Center(child: Text('Welcome to Home Screen')),
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        centerTitle: true,
+        backgroundColor: Colors.white,
+        title: const Text(
+          appName,
+          style: TextStyle(
+              color: Colors.blue, fontSize: 20, fontWeight: FontWeight.w500),
+        ),
+        actions: [
+          IconButton(
+              padding: const EdgeInsets.only(right: 10),
+              onPressed: () {},
+              icon: const Icon(
+                Icons.brightness_4_rounded,
+                color: Colors.blue,
+                size: 26,
+              ))
+        ],
+      ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(
+            horizontal: mq.width * .04, vertical: mq.height * .015),
+        children: HomeType.values
+            .map((e) => HomeCard(
+                  homeType: e,
+                ))
+            .toList(),
+      ),
     );
   }
 }
